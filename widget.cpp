73debe1f304160bf6b05927650b58694673761d8
unknown <1062826362@qq.com>
2019-05-21 12:49:14 +0800

    将文本框替换为对话框，增加图片

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QMouseEvent>
+#include <QMessageBox>
+
 QRect buttonlike,buttondislike,button;
 
 Widget::Widget(QWidget *parent) :
@@ -9,6 +11,10 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
     setMouseTracking(true);
+    QPixmap picture;
+    picture.load("65.jpg");
+    ui->label->setText("做我女朋友吧");
+    ui->label_2->setPixmap(picture);
     buttonlike = ui->Buttonlike->geometry();
     buttondislike = ui->Buttondislike->geometry();
 }
@@ -20,12 +26,26 @@ Widget::~Widget()
 
 void Widget::on_Buttondislike_clicked()
 {
-    ui->textBrowser->append("你这个狗子竟然不同意");
+    QMessageBox *msgBox;
+    msgBox = new QMessageBox("伤心",                     //--这里是设置消息框标题
+            "你这个狗子竟然不同意",                        //--这里是设置消息框显示的内容
+            QMessageBox::Critical,                      //--这里是在消息框显示的图标
+            QMessageBox::Ok | QMessageBox::Default,     //---这里是显示消息框上的按钮情况
+            QMessageBox::Cancel | QMessageBox::Escape,	//---这里与 键盘上的 escape 键结合。当用户按下该键，消息框将执行cancel按钮事件
+            0);
+    msgBox->show();
 }
 
 void Widget::on_Buttonlike_clicked()
 {
-   ui->textBrowser->append("我就知道你会选这个的");
+   QMessageBox *msgBox;
+   msgBox = new QMessageBox("开心",                     //--这里是设置消息框标题
+           "太好了你竟然同意了",                        //--这里是设置消息框显示的内容
+           QMessageBox::Critical,                      //--这里是在消息框显示的图标
+           QMessageBox::Ok | QMessageBox::Default,     //---这里是显示消息框上的按钮情况
+           QMessageBox::Cancel | QMessageBox::Escape,	//---这里与 键盘上的 escape 键结合。当用户按下该键，消息框将执行cancel按钮事件
+           0);
+   msgBox->show();
 }
 
 void Widget::mouseMoveEvent(QMouseEvent *event)
